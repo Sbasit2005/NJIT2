798090bb9be5377964d0ad2f3be2a32177219fdd
Sbasit2005 <92735685+Sbasit2005@users.noreply.github.com>
2023-02-24 13:08:21 -0500

    Final Commit
    trying to fix GitHUb issues

--- js/gallery.js
+++ js/gallery.js
@@ -34,10 +34,62 @@ function animate() {
 
 function swapPhoto() {
 	//Add code here to access the #slideShow element.
+	
 	//Access the img element and replace its source
 	//with a new image from your images array which is loaded 
 	//from the JSON string
-	console.log('swap photo');
+	
+if(mCurrentIndex >= mImages.length) {
+		mCurrentIndex = 0;
+}
+
+if(mCurrentIndex < 0) {
+		mCurrentIndex = mImages.length;
+}
+
+document.getElementById('photo').src = mImages[mCurrentIndex].img;
+
+var loc = document.getElementsByClassName('number');
+loc[0].innerHTML = 'number: ' + mImages[mCurrentIndex].number;
+
+var des = document.getElementsByClassName('description');
+des[0].innerHTML = "description: " + mImages[mCurrentIndex].description;
+
+var dt = document.getElementsByClassName('date');
+dt[0].innerHTML = "date: " + mImages[mCurrentIndex].date;
+
+mLastFrameTime = 0
+mCurrentIndex += 1
+}
+
+function prevPhoto() {
+	//Add code here to access the #slideShow element.
+	
+	//Access the img element and replace its source
+	//with a new image from your images array which is loaded 
+	//from the JSON string
+	
+if(mCurrentIndex >= mImages.length) {
+		mCurrentIndex = 0;
+}
+
+if(mCurrentIndex < 0) {
+		mCurrentIndex = mImages.length;
+}
+
+document.getElementById('photo').src = mImages[mCurrentIndex].img;
+
+var loc = document.getElementsByClassName('number');
+loc[0].innerHTML = 'number: ' + mImages[mCurrentIndex].number;
+
+var des = document.getElementsByClassName('description');
+des[0].innerHTML = "description: " + mImages[mCurrentIndex].description;
+
+var dt = document.getElementsByClassName('date');
+dt[0].innerHTML = "date: " + mImages[mCurrentIndex].date;
+
+mLastFrameTime = 0
+mCurrentIndex -= 1
 }
 
 // Counter for the mImages array
@@ -54,10 +106,34 @@ var mJson;
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl;
+var mUrl = 'https://api.npoint.io/6df0e6fb32474375e6fc';
 
-mUrl = 'images.json';
 
+
+
+// part 2 slideshow 1 & 2 convetrs data to readable txt
+function fetchJSON () {
+	mRequest.onreadystatechange = function() {
+		console.log("on ready state change");
+		if (this.readyState == 4 && this.status == 200) {
+			mJson = JSON.parse(mRequest.responseText);
+			console.log('erm');
+			iterateJSON(mJson);
+		}		
+	}
+	mRequest.open("GET", mUrl,true);
+	mRequest.send();
+}
+
+function iterateJSON(mJson) {
+	for (x = 0; x < mJson.images.length; x++) {
+		mImages[x] = new GalleryImage();
+		mImages[x].number = mJson.images[x].imgNumber;
+		mImages[x].description = mJson.images[x].description;
+		mImages[x].date = mJson.images[x].date;
+		mImages[x].img = mJson.images[x].imgPath;
+	}
+}
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
 function makeGalleryImageOnloadCallback(galleryImage) {
@@ -68,10 +144,35 @@ function makeGalleryImageOnloadCallback(galleryImage) {
 }
 
 $(document).ready( function() {
-	
+
+	$('#nextPhoto').position({
+		my: 'right bottom',
+		at: 'right bottom',
+		of: '#nav'
+	})
+
+const urlParams = new URLSearchParams(window.location.search)
+
+	for (const [key, value] of urlParams) {
+		console.log(`${key}:${Value}`)
+		mUrl = value
+	}
+
+	if (mUrl == undefined) {
+		mUrl = 'https://api.npoint.io/6df0e6fb32474375e6fc';
+	}
+
+ fetchJSON();
+	$("#nextPhoto").click(function () {
+		swapPhoto()
+	});
+
+	$("#prevPhoto").click(function () {
+		swapPhoto()
+	});
 	// This initially hides the photos' metadata information
-	$('.details').eq(0).hide();
-	
+	//$('.details').eq(0).hide();
+
 });
 
 window.addEventListener('load', function() {
@@ -79,11 +180,11 @@ window.addEventListener('load', function() {
 	console.log('window loaded');
 
 }, false);
-
+// Assigning data from JSON list to variables that will be used in slideshow
 function GalleryImage() {
 	//implement me as an object to hold the following data about an image:
-	//1. location where photo was taken 
-	var location;
+	//1. number where photo was taken 
+	var number;
 	//2. description of photo
 	var description;
 	//3. the date when the photo was taken
@@ -91,3 +192,23 @@ function GalleryImage() {
 	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
 	var img;
 }
+
+function Rswitch() {
+	if ($(".moreIndicator").hasClass("rot90")){
+		$(".moreIndicator").addClass("rot270")
+		$(".moreIndicator").removeClass("rot90")
+	}
+	else{
+		$(".moreIndicator").addClass("rot90")
+		$(".moreIndicator").removeClass("rot270")
+	}
+	$(".details").slideToggle("slow");
+}
+
+
+
+$( "#clickme" ).click(function() {
+  $( "#book" ).slideToggle( "slow", function() {
+    // Animation complete.
+  });
+})
